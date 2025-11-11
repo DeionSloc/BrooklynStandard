01e409bfbf1a8ac585c4674ab6defd7ca2e2bda2
DeionSloc <95542183+DeionSloc@users.noreply.github.com>
2025-11-10 17:58:20 -0700

    Create project
    Added '.gitignore file'
    Created .Net Proejct and added folder to cloned repository

---
+++ BrooklynStandard/BrooklynStandard/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace BrooklynStandard.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
